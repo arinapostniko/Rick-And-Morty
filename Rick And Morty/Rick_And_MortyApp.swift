3dc4974384ab6bccb022766bd736f6d08cfd292e
Arina <arinapostnikova123@gmail.com>
2023-08-17 10:27:49 +0300

    Initial commit

---
+++ Rick And Morty/Rick_And_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_MortyApp.swift
+//  Rick And Morty
+//
+//  Created by Arina Postnikova on 17.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
